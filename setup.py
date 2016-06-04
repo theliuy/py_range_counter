da1662a65d406c12543812cdf66fc698316cb700
Yang Liu <liuyang.09@bytedance.com>
2016-06-04 08:32:53 +0800

    Added setup.
    Change-Id: Ida333e9de98dc02673fdd4c92b995d58151acdbf

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='py_range_counter',
+      version='0.1',
+      description='Python Range Counter',
+      author='Yang Liu',
+      author_email='liuyruc@gmail.com',
+      packages=['py_range_counter'],
+     )
